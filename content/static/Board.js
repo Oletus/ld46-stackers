72ec0fe653b28e08f67dd2c9bca70d800976289f
XCompWiz <xcompwiz@gmail.com>
2020-04-19 12:46:12 +0300

    Add gold sprite and reorder sprite defines

--- content/static/Board.js
+++ content/static/Board.js
@@ -14,19 +14,23 @@ class Board {
   constructor(ctx) {
     this.ctx = ctx;
     this.domino_base = new Sprite('domino_base.png');
+
+    this.domino_t_gold = new Sprite('domino_base.png');
+    this.domino_bl_gold = new Sprite('domino_bottom_left-red.png');
+    this.domino_br_gold = new Sprite('domino_bottom_right-red.png');
     this.domino_t_red = new Sprite('domino_top-red.png');
-    this.domino_t_green = new Sprite('domino_top-green.png');
-    this.domino_t_blue = new Sprite('domino_top-blue.png');
     this.domino_bl_red = new Sprite('domino_bottom_left-red.png');
-    this.domino_bl_green = new Sprite('domino_bottom_left-green.png');
-    this.domino_bl_blue = new Sprite('domino_bottom_left-blue.png');
     this.domino_br_red = new Sprite('domino_bottom_right-red.png');
+    this.domino_t_green = new Sprite('domino_top-green.png');
+    this.domino_bl_green = new Sprite('domino_bottom_left-green.png');
     this.domino_br_green = new Sprite('domino_bottom_right-green.png');
+    this.domino_t_blue = new Sprite('domino_top-blue.png');
+    this.domino_bl_blue = new Sprite('domino_bottom_left-blue.png');
     this.domino_br_blue = new Sprite('domino_bottom_right-blue.png');
 
-    this.domino_tops = [this.domino_t_red, this.domino_t_green, this.domino_t_blue];
-    this.domino_bottom_lefts = [this.domino_bl_red, this.domino_bl_green, this.domino_bl_blue];
-    this.domino_bottom_rights = [this.domino_br_red, this.domino_br_green, this.domino_br_blue];
+    this.domino_tops = [this.domino_t_gold, this.domino_t_red, this.domino_t_green, this.domino_t_blue];
+    this.domino_bottom_lefts = [this.domino_bl_gold, this.domino_bl_red, this.domino_bl_green, this.domino_bl_blue];
+    this.domino_bottom_rights = [this.domino_br_gold, this.domino_br_red, this.domino_br_green, this.domino_br_blue];
   }
 
   drawGrid(state) {
