bc92bfa3ec6871014b26237d7259714b36e5810e
XCompWiz <xcompwiz@gmail.com>
2020-04-19 14:40:03 +0300

    Add UI layout struct definition to simplify and improve control over UI layout

--- content/static/Board.js
+++ content/static/Board.js
@@ -3,6 +3,12 @@ import { Sprite } from "./gameutils.js/src/gjs/sprite.js";
 Sprite.gfxPath = '/';
 
 
+const layout = {
+  board: {offset: {x: 0, y: 0}},
+  deck: {offset: {x: 0, y: 0}, padding: {x: 10, y: 4}},
+  domino: {width: 64, height: 32},
+}
+
 class Board {
   constructor(ctx) {
     this.ctx = ctx;
@@ -22,6 +28,12 @@ class Board {
     this.domino_tops = [this.domino_t_gold, this.domino_t_red, this.domino_t_green, this.domino_t_blue];
     this.domino_bottom_lefts = [this.domino_bl_red, this.domino_bl_red, this.domino_bl_green, this.domino_bl_blue];
     this.domino_bottom_rights = [this.domino_bl_red, this.domino_br_red, this.domino_br_green, this.domino_br_blue];
+    
+    layout.domino.width = this.domino_base.img.width;
+    layout.domino.height = this.domino_base.img.height;
+    layout.board.offset.x = layout.domino.width / 2;
+    layout.deck.offset.x = layout.domino.width / 2;
+    layout.deck.offset.y = layout.domino.height * 7.5;
   }
 
   drawGrid(state) {
@@ -29,18 +41,18 @@ class Board {
     this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
     for (var row = 0; row < grid.length; ++row) {
       for (var slotI = 0; slotI < grid[row].length; ++slotI) {
-        let x = (grid.length - row) * (this.domino_base.img.width / 2) + (slotI * this.domino_base.img.width);
-        let y = (row + 1) * this.domino_base.img.height;
-        this.ctx.strokeRect(x, y, this.domino_base.img.width, this.domino_base.img.height);
+        let x = layout.board.offset.x + (grid.length - row) * (layout.domino.width / 2) + (slotI * layout.domino.width);
+        let y = layout.board.offset.y + (row + 1) * layout.domino.height;
+        this.ctx.strokeRect(x, y, layout.domino.width, layout.domino.height);
       }
     }
   }
   
   drawDomino(domino, x, y) {
-    this.domino_base.draw(this.ctx, x, y);
-    this.domino_tops[domino.primary].draw(this.ctx, x, y);
-    this.domino_bottom_lefts[domino.left].draw(this.ctx, x, y);
-    this.domino_bottom_rights[domino.right].draw(this.ctx, x, y);
+    this.domino_base.draw(this.ctx, x, y, layout.domino.width, layout.domino.height);
+    this.domino_tops[domino.primary].draw(this.ctx, x, y, layout.domino.width, layout.domino.height);
+    this.domino_bottom_lefts[domino.left].draw(this.ctx, x, y, layout.domino.width, layout.domino.height);
+    this.domino_bottom_rights[domino.right].draw(this.ctx, x, y, layout.domino.width, layout.domino.height);
   }
 
   drawPieces(state) {
@@ -62,15 +74,14 @@ class Board {
         if (domino === undefined)
           continue;
 
-        var x = (grid.length - row) * (this.domino_base.img.width / 2) + (slotI * this.domino_base.img.width);
-        var y = (row + 1) * this.domino_base.img.height;
+        var x = layout.board.offset.x + (grid.length - row) * (layout.domino.width / 2) + (slotI * layout.domino.width);
+        var y = layout.board.offset.y + (row + 1) * layout.domino.height;
         this.drawDomino(domino, x, y);
       }
     }
   }
 
   drawDeck(state, playerId) {
-    var deckOffset = {x: this.domino_base.img.width / 2, y: this.domino_base.img.height * 7.5}
     var deck = state.decks[playerId];
     for (var slotI = 0; slotI < deck.length; ++slotI) {
       var domino = deck[slotI];
@@ -84,8 +95,8 @@ class Board {
       var x = slotI % 8;
       var y = Math.floor(slotI / 8);
       
-      var x = x * (this.domino_base.img.width + 10) + deckOffset.x;
-      var y = y * (this.domino_base.img.height + 4) + deckOffset.y;
+      var x = layout.deck.offset.x + x * (layout.domino.width + layout.deck.padding.x);
+      var y = layout.deck.offset.y + y * (layout.domino.height + layout.deck.padding.y);
       this.drawDomino(domino, x, y);
     }
   }
